261587b8cd3727ceee59aedd2fce85e082aba3b4
Branum <jcb179@txstate.edu>
2019-09-23 19:38:03 -0500

    Completed Task 4 Operators

--- javascript/external.js
+++ javascript/external.js
@@ -13,5 +13,12 @@
 
 
 //A prompt box is used to promt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here")
-document.write(user_name);
+//user_name = window.prompt("Please enter your name", "Type your name here")
+//document.write(user_name);
+
+
+x = 22;
+const y = 33;
+document.writeln(x + y);
+document.writeln("<br>");
+document.writeln(x += 33);
