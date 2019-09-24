786bfb2d143a85d9f9f1800f8259dd5993ad5ad1
Branum <jcb179@txstate.edu>
2019-09-23 19:33:46 -0500

    Completed Task 3 Varialbes

--- javascript/external.js
+++ javascript/external.js
@@ -1,1 +1,17 @@
-document.write("where is my JS code?");
+//document.write("where is my JS code?");
+
+//window.alert(5 + 6);
+
+//document.write('<h1>A heading</h>');
+//document.write('<p>A sentence</p>');
+
+
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out);
+
+
+//A prompt box is used to promt users to input a value before entering a page.
+user_name = window.prompt("Please enter your name", "Type your name here")
+document.write(user_name);
